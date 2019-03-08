976d9ca605ebb7aa3d7afb63b20966b5466bed4f
Sahir Hoda <shoda@tibco.com>
2019-03-08 06:59:13 -0600

    add hundred and thousand preset buttons, remove firebase js, remove caching, UI

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import random from 'random'
 
-function numbwExec () {
-  const lowE = document.querySelector('#numbw-low')
-  const highE = document.querySelector('#numbw-high')
-  const outE = document.querySelector('#numbw-out')
-
-  const low = parseInt(lowE.value)
-  const high = parseInt(highE.value)
-  const rand = random.int(low, high)
-  outE.innerHTML = rand
+// Globals
+const numbw = {
+  e: {
+    low: document.querySelector('#numbw-low'),
+    high: document.querySelector('#numbw-high'),
+    out: document.querySelector('#numbw-out'),
+    execBtn: document.querySelector('#numbw-exec'),
+    hundredBtn: document.querySelector('#numbw-preset-hundred'),
+    thousandBtn: document.querySelector('#numbw-preset-thousand')
+  }
 }
 
+// Functions
+function numbwExec () {
+  const low = parseInt(numbw.e.low.value)
+  const high = parseInt(numbw.e.high.value)
+  // TODO validation
+  const rand = random.int(low, high)
+  numbw.e.out.innerHTML = rand
+}
+
+// Main
 function main () {
-  document.querySelector('#numbw-exec').addEventListener('click', numbwExec, false)
+  // wire up
+  numbw.e.execBtn.addEventListener('click', numbwExec, false)
+  numbw.e.hundredBtn.addEventListener('click', function () {
+    numbw.e.low.value = 1
+    numbw.e.high.value = 100
+    numbwExec()
+  }, false)
+  numbw.e.thousandBtn.addEventListener('click', function () {
+    numbw.e.low.value = 1
+    numbw.e.high.value = 1000
+    numbwExec()
+  }, false)
 }
 
 main()
